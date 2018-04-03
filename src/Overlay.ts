9898622f58360078708d460eb87ac578724e02b7
wf <wangfeng@iri.com>
2018-04-03 11:22:51 +0800

    fix code style

--- src/Overlay.ts
+++ src/Overlay.ts
@@ -11,11 +11,12 @@ export interface OverlayProperties {
 
 // 注意，Overlay 只能放在原子部件上，不能放在容器部件上。
 export default class Overlay extends WidgetBase<OverlayProperties> {
-	protected render():VNode {
+	protected render(): VNode {
 		const { dimensions } = this.properties;
-		return v('div',
-{
-	classes: css.root,
+		return v(
+			'div',
+			{
+				classes: css.root,
 				style: `top: 0px; 
                 left: 0px; 
                 height: ${dimensions.size.height}px; 
