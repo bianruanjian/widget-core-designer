a2fed6ffcba6d5566f82e185857633dcb052dee0
wf <wangfeng@iri.com>
2018-05-18 17:19:17 +0800

    deal with properties

--- src/DesignerWidgetMixin.ts
+++ src/DesignerWidgetMixin.ts
@@ -1,7 +1,6 @@
 import { UIInstWidget } from "./interfaces";
 import { Dimensions, DimensionResults } from '@dojo/widget-core/meta/Dimensions';
 import { afterRender } from '@dojo/widget-core/decorators/afterRender';
-import { beforeRender } from '@dojo/widget-core/decorators/beforeRender';
 import { Constructor, DNode, VNode } from '@dojo/widget-core/interfaces';
 import { WidgetBase } from '@dojo/widget-core/WidgetBase';
 import { beforeProperties } from '@dojo/widget-core/decorators/beforeProperties';
@@ -25,13 +24,14 @@ export interface DesignerWidgetMixin {
 export function DesignerWidgetMixin<T extends new (...args: any[]) => WidgetBase>(Base: T): T & Constructor<DesignerWidgetMixin> {
 	abstract class Designable extends Base {
 		public abstract properties: EditableWidgetProperties;
+		
 
-		private _dimensions: DimensionResults|undefined;
+		private _key:string  ='';
 
 		private _onMouseUp(event: MouseEvent) {
 			event.stopImmediatePropagation();
 			const { onFocus, widget } = this.properties;
-			const dimensions = this.meta(Dimensions).get(String(this.properties.key));
+			const dimensions = this.meta(Dimensions).get(this._key);
 			onFocus && onFocus({ activeWidgetDimensions: dimensions, activeWidgetId: widget.id });
 		}
 
@@ -43,33 +43,20 @@ export function DesignerWidgetMixin<T extends new (...args: any[]) => WidgetBase
 			return false;
 		}
 
-		private get dimensions() {
-			if (this._dimensions) {
-				return this._dimensions;
-			}
-			return this.meta(Dimensions).get(String(this.properties.key));
-		}
-
 		@beforeProperties()
-		protected beforeProperties() {
-			if (!this.properties.widget) {
-				return { ...this.properties };
+		protected beforeProperties(properties:any) {
+			if (!properties.widget) {
+				return { ...properties };
 			}
 			// 如果是空容器，则添加可视化效果
 			if (this.isContainer() && this.children.length < 1) {
 				return {
-					extraClasses: { 'root': css.emptyContainer }, ...this.properties, ...this.properties.widget.properties
+					extraClasses: { 'root': css.emptyContainer }, ...properties, ...properties.widget.properties
 				}
 			} 
 			return {
-				...this.properties, ...this.properties.widget.properties
+				...properties, ...properties.widget.properties
 			}
-		}
-
-		@beforeRender()
-		protected beforeRender(){
-			const { widget, activeWidgetId, onFocus } = this.properties;
-			this._tryFocus(widget, activeWidgetId, onFocus);
 		}
 
 		// 1. 尝试聚焦
@@ -77,20 +64,26 @@ export function DesignerWidgetMixin<T extends new (...args: any[]) => WidgetBase
 		// 3. input部件需要增加遮盖层节点
 		@afterRender()
 		protected afterRender(result: DNode | DNode[]): DNode | DNode[] {
-			const key = String(this.properties.key);
 			// 若为虚拟节点数组需要遍历所有节点，找到应用了key的节点，再添加onmouseup事件
+			let key :string; 
 			if (Array.isArray(result)) {
 				result = result as DNode[];
 				let node = find(result, (elm, index, array) => {
-					return (elm as VNode).properties.key === key;
+					return typeof (elm as VNode).properties.key !== undefined;
 				});
 				(node as VNode).properties.onmouseup = this._onMouseUp;
+				key = String((node as VNode).properties.key);
 			} else {
 				(result as VNode).properties.onmouseup = this._onMouseUp;
+				key = String((result as VNode).properties.key);
 				result = [result];
 			}
+			this._key =key;
+
+			const { widget, activeWidgetId, onFocus } = this.properties;
+			this._tryFocus(widget, activeWidgetId, onFocus, key);
 			if (this.needOverlay()) {
-				return [...result, w(Overlay, { dimensions: this.dimensions })]
+				return [...result, w(Overlay, { dimensions: this.meta(Dimensions).get(key) })]
 			}
 			return [...result];
 		}
@@ -107,10 +100,11 @@ export function DesignerWidgetMixin<T extends new (...args: any[]) => WidgetBase
 					activeWidgetDimensions: Readonly<DimensionResults>;
 					activeWidgetId: string | number;
 				}
-			) => void
+			) => void,
+			key: string
 		) {
 			if (this._isFocus(widget, activeWidgetId)) {
-				this._focus(onFocus, activeWidgetId);
+				this._focus(onFocus, activeWidgetId,key);
 			}
 		}
 
@@ -120,11 +114,10 @@ export function DesignerWidgetMixin<T extends new (...args: any[]) => WidgetBase
 					activeWidgetDimensions: Readonly<DimensionResults>;
 					activeWidgetId: string | number;
 				}
-			) => void, activeWidgetId: string | number){
+			) => void, activeWidgetId: string | number,key:string){
 				const { widget } = this.properties;
-				const dimensions = this.meta(Dimensions).get(String(this.properties.key));
-				this._dimensions = dimensions;
-				this.meta(Resize).get(String(this.properties.key),{});
+				const dimensions = this.meta(Dimensions).get(key);
+				this.meta(Resize).get(String(key),{});
 				onFocus && onFocus({ activeWidgetDimensions: dimensions, activeWidgetId: widget.id });
 		}
 
