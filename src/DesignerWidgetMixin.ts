407c60eccccb4e74ec8406491b5efa6c0e79e183
jfx <jiangfx@iri.cn>
2018-09-21 15:08:35 +0800

    #634 add notes

--- src/DesignerWidgetMixin.ts
+++ src/DesignerWidgetMixin.ts
@@ -77,8 +77,8 @@ export function DesignerWidgetMixin<T extends new (...args: any[]) => WidgetBase
 			if (this.isContainer() && (this.children.length === 0 || this._onlyContainsCursor())) {
 				return {
 					extraClasses: { root: css.emptyContainer },
-					...properties.widget.properties,
-					...properties
+					...properties.widget.properties, // 设计器中初始化默认的或者通过属性面板修改的属性值
+					...properties // 使用 w() 创建部件时传入的属性
 				};
 			}
 			// 存在这么一类部件，不属于容器部件，也不需要遮盖层，支持 value 属性，当 value 值为空且不存在光标之外的子部件的时候需要设置 value 为 '__'
@@ -86,14 +86,14 @@ export function DesignerWidgetMixin<T extends new (...args: any[]) => WidgetBase
 				// 在设计器中，使用 value 覆盖掉 properties.widget.properties 中的 value 属性值。
 				// 但是并不会往 web 版的部件中传修改后的 value 值，还是传 properties.widget.properties 中的 value 值。
 				return {
-					...properties.widget.properties,
-					...properties,
+					...properties.widget.properties, // 设计器中初始化默认的或者通过属性面板修改的属性值
+					...properties, // 使用 w() 创建部件时传入的属性
 					value: this.getDefaultValue()
 				};
 			}
 			return {
-				...properties.widget.properties,
-				...properties
+				...properties.widget.properties, // 设计器中初始化默认的或者通过属性面板修改的属性值
+				...properties // 使用 w() 创建部件时传入的属性
 			};
 		}
 
